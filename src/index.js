93a5a0a4117fc95bbaab37de6d7c3e2eaf903c62
sgmeklvich <meklvich@proton.me>
2026-08-15 16:52:04 +0800

    Refactor ReactDOM rendering to use createRoot
    Updated ReactDOM rendering to use createRoot for concurrent mode.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* old
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,6 +6,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+*/
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
