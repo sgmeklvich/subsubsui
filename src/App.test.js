572fb9915398260003ea1450743dd5a5390c7253
sgmeklvich <meklvich@proton.me>
2026-08-16 13:37:51 +0800

    Delete src directory

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders digitalocean docs', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/DigitalOcean Docs/i);
-  expect(linkElement).toBeInTheDocument();
-});
