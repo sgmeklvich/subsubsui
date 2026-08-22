1cda5fed43350ddab83fd309b48dbdadbcb92fce
sgmeklvich <meklvich@proton.me>
2026-08-22 17:24:58 +0800

    Remove CA certificate from database connection config
    Removed CA certificate configuration from database connection.

--- server/db.js
+++ server/db.js
@@ -5,7 +5,6 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // This bypasses the self-signed certificate error
-    ca: process.env.CA_CERT,
   }
 });
 
